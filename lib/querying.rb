d43c4fb1abcfb143d29be3bb9cfc21f54a3bfd50
Yuliya Marinova <yuliyambg@gmail.com>
2020-09-27 04:33:35 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -39,5 +39,9 @@ LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name, COUNT(*) as book_count 
+FROM character_books 
+JOIN characters ON character_books.character_id = characters.id 
+GROUP BY characters.name 
+ORDER BY book_count DESC;"
 end
