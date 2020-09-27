9f683d72804e3952aca31f0364b19ee4d33990e5
Yuliya Marinova <yuliyambg@gmail.com>
2020-09-27 03:45:53 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -5,3 +5,5 @@ INSERT INTO books (title, year, series_id) VALUES
 ("Game of Thrones", 1996, 1), ("A Clash of Kings", 1998, 1), 
 ("A Storm of Swords", 2000, 1), ("First Book", 2002, 2),
  ("Second Book", 2003, 2), ("Third Book", 2006, 2);
+ 
+ 
