2421b1c609c3773f2a2f65b7f7b6d8d709b6999b
Yuliya Marinova <yuliyambg@gmail.com>
2020-09-27 04:32:12 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -29,7 +29,13 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title FROM series 
+JOIN books ON books.series_id = series.id 
+JOIN character_books ON character_books.book_id = books.id 
+JOIN characters ON character_books.character_id = characters.id WHERE characters.species = 'human' 
+GROUP BY series.title 
+ORDER BY COUNT(*) DESC 
+LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
