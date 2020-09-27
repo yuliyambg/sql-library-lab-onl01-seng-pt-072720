1bd0cf0a99a3e5db1a201d14157f5dc85e47b3b3
Yuliya Marinova <yuliyambg@gmail.com>
2020-09-27 04:30:12 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,7 +20,12 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name, subgenres.name 
+   FROM authors 
+   JOIN series 
+   ON series.author_id = authors.id 
+   JOIN subgenres 
+   ON series.subgenre_id = subgenres.id;"
 end
 
 def select_series_title_with_most_human_characters
