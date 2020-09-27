e827cc3a13f3d3a845bbf3eb5ce6bdb000afdf58
Yuliya Marinova <yuliyambg@gmail.com>
2020-09-27 03:51:42 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -7,3 +7,11 @@ INSERT INTO books (title, year, series_id) VALUES
  ("Second Book", 2003, 2), ("Third Book", 2006, 2);
  
  
+ INSERT INTO characters (name, motto, species, author_id) VALUES 
+("Lady", "Woof Woof", "direwolf", 1), ("Tyrion Lannister", "A Lannister always pays his debts", "human", 1),
+("Daenerys Targaryen", "If I look back I am lost", "human", 1), ("Eddard Stark", "Winter is coming", "human", 1);
+
+INSERT INTO characters (name, motto, species, author_id) VALUES
+("Character One", "motto one", "cylon", 2), ("Character Two", "motto two", "human", 2),
+("Character Three", "motto three", "cylon", 2), ("Character Four", "motto four", "cylon", 2);
+ 
