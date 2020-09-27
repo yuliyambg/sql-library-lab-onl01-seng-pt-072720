47396732dc5764028c4211cbc9a044b4ba261733
Yuliya Marinova <yuliyambg@gmail.com>
2020-09-27 04:25:44 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -4,7 +4,10 @@ def select_books_titles_and_years_in_first_series_order_by_year
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT characters.name, characters.motto 
+  FROM characters 
+  ORDER BY LENGTH(motto) DESC 
+  LIMIT 1;"
 end
 
 
