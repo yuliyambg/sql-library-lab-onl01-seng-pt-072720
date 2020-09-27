65e2d5e36195a1f33603a3c267dafbc899426ccf
Yuliya Marinova <yuliyambg@gmail.com>
2020-09-27 04:27:19 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,7 +12,11 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT species, COUNT (*) 
+  FROM characters 
+  GROUP BY species 
+  ORDER BY COUNT (species) DESC 
+  LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
